591bf33bcbea9da246a9bbc923c0a3d8a9e1e715
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2023-05-02 22:11:38 -0500

    Cancel button

--- src/components/UploadPDF/UploadPDF.tsx
+++ src/components/UploadPDF/UploadPDF.tsx
@@ -8,16 +8,18 @@ const UploadPDF: React.FC = () => {
       );
     const [technology, setTechnology] = useState<string>('Python');
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [fileName, setFileName] = useState<string>("");
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
             const selectedFile = event.target.files[0];
-
             if (selectedFile.type === "application/pdf") {
                 setFile(selectedFile);
+                setFileName(selectedFile.name);
                 setErrorMessage(null);
             } else {
                 setFile(null);
+                setFileName("");
                 setErrorMessage("Please select a PDF file.");
             }
         }
@@ -49,6 +51,12 @@ const UploadPDF: React.FC = () => {
                     });
             };
         }
+    };
+
+    const removeFile = () => {
+        setFileName("");
+        setFile(null);
+        setCurrentFile(null);
     };
 
     return (
@@ -88,13 +96,14 @@ const UploadPDF: React.FC = () => {
                                 accept="application/pdf"
                                 onChange={handleFileChange} />
                         </label>
+
                         {errorMessage && (
                         <p className="mt-2 text-sm text-red-600" id="file-validation-error">
                             {errorMessage}
                         </p>
                         )}
                     </div>
-                </div>
+                </div>    
 
                 {currentFile && (
                      <embed src={currentFile.toString()} width="300" height="500" type='application/pdf'></embed>
@@ -109,6 +118,16 @@ const UploadPDF: React.FC = () => {
                     Upload
                 </button>
                 </div>
+                <div>
+                    {file && (
+                        <button onClick={() => removeFile()}
+                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#FA654F] hover:bg-[#FAA69B] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        >
+                            Cancel
+                        </button>
+                    )}
+                </div>
+                
             </form>
             </div>
         </div>
